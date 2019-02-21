3809384e66228ac7d5d2588a8ea32a778f046673
priyaamithun <veerpriyaa@gmail.com>
2019-02-21 17:14:00 -0600

    updated 7

--- src/Components/Contactus.jsx
+++ src/Components/Contactus.jsx
@@ -4,18 +4,18 @@ import './Contactus.css'
 const Contactus = () =>(
 <div className="contact-content" id="contact">
   <h1>Contact Us</h1>
-<hr/>
+<hr/> 
 <form className="form-contentnew">
   <div className="form-group">
     
-    <input type="email" className="form-control" placeholder="Email address"/>
+    <input type="email" className="form-control" placeholder="Email address" required/>
   </div>
   <div className="form-group">
     
-    <input type="email" className="form-control" placeholder="Full name"/>
+    <input type="email" className="form-control" placeholder="Full name" required/>
   </div>
   <br/>
-  <textarea  className="textarea" rows="4" cols="75" defaultValue="Your Message....." />
+  <textarea  className="textarea" rows="4" cols="75" defaultValue="Your Message....."  required/>
 <br/>
 <br/>
   <button type="submit" className="btn btn-secondary">Submit</button>
